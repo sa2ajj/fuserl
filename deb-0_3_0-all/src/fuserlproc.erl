664579e2ce012b90159d7610205d61535ab3fc51
paul-google@mineiro.com <paul-google@mineiro.com@71cd72d2-da42-0410-ba85-fd6e4b52f218>
2008-03-05 04:48:12 +0000

    tag 'deb-0_3_0-all' created by fw-revision-svn

---
+++ deb-0_3_0-all/src/fuserlproc.erl
@@ -0,0 +1,45 @@
+-module (fuserlproc).
+-behaviour (application).
+-export ([ start/0,
+           start/2,
+           stop/0,
+           stop/1 ]).
+
+%-=====================================================================-
+%-                        application callbacks                        -
+%-=====================================================================-
+
+%% @hidden
+
+start () ->
+  application:start (fuserl),
+  application:start (fuserlproc).
+
+%% @hidden
+
+start (_Type, _Args) ->
+  { ok, LinkedIn } = application:get_env (fuserlproc, linked_in),
+  { ok, MountPoint } = application:get_env (fuserlproc, mount_point),
+  case application:get_env (fuserlproc, make_mount_point) of
+    { ok, false } -> 
+      ok;
+    _ ->
+      case file:make_dir (MountPoint) of
+        ok ->
+          ok;
+        { error, eexist } ->
+          ok
+      end
+  end,
+
+  fuserlprocsup:start_link (LinkedIn, MountPoint).
+
+%% @hidden
+
+stop () ->
+  application:stop (fuserlproc).
+
+%% @hidden
+
+stop (_State) ->
+  ok.
